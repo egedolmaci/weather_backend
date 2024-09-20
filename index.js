87bcb918c6f29cdba464641e0246e4ce59f8c8f8
egedolmaci <dolmaciege@gmail.com>
2024-09-21 00:16:01 +0300

    added different file routing

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
-// import weatherRoute from "./routes/weather.js";
+import weatherRoute from "./routes/weather.js";
 import weather from "./controllers/weather.js";
 
 const app = express();
 
 app.use(express.json());
-
-// app.use("/weather", weatherRoute);
-app.get("/daily", weather.getDaily);
-
-app.get("/hourly", weather.getHourly);
+app.use("/weather", weatherRoute);
 
 const PORT = process.env.PORT || 5000;
 
